af68a35a4fe16c39b595481e0a10e87184ca584e
Pavan Kunchala <pavankuchalapk@gmail.com>
2020-07-24 17:44:15 +0530

    created a basic a neural network
    to predict the relation between X and Y

---
+++ Basics/Basics_part1.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+import numpy as np
+
+from tensorflow import keras
+
+#defining a model
+
+model = tf.keras.Sequential([keras.layers.Dense(units = 1,input_shape =[1])])
+
+
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+
+xs = np.array([-1.0,  0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
+
+
+#training A neural network
+
+model.fit(xs, ys, epochs=500)
+
+#prediction of the model
+
+print("The prediction is ",model.predict([10.0]))
