1040e89d0bcac23018d9c759e2b1e1879dd08ab0
Pavan Kunchala <pavankuchalapk@gmail.com>
2020-09-26 15:31:19 +0530

    Haar cascading
    We created a model to compare some possible cascades like defaut, alt2 ,and haar_default using openCV

--- Face-Detection-OpenCV/face_detection_harr.py
+++ Face-Detection-OpenCV/face_detection_harr.py
@@ -6,3 +6,74 @@ Created on Sat Sep 26 14:37:39 2020
 @author: pavankunchala
 """
 
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def show_img_with_matplotlib(color_img, title, pos):
+    """Shows an image using matplotlib capabilities"""
+
+    img_RGB = color_img[:, :, ::-1]
+
+    ax = plt.subplot(2, 2, pos)
+    plt.imshow(img_RGB)
+    plt.title(title)
+    plt.axis('off')
+
+def show_detection(image,faces):
+    
+    for(x,y,w,h) in faces:
+        cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),5)
+    return image
+
+
+#load the image
+img = cv2.imread("face.jpg")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#loading the cascade classifiers
+cas_alt2 = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+cas_default = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+#detect faces
+faces_alt2  = cas_alt2.detectMultiScale(gray)
+faces_default =cas_default.detectMultiScale(gray)
+
+retval , faces_haar_alt2 = cv2.face.getFacesHAAR( img, "haarcascade_frontalface_alt2.xml")
+faces_haar_alt2 = np.squeeze(faces_haar_alt2)
+
+retval , faces_haar_default = cv2.face.getFacesHAAR( img, "haarcascade_frontalface_default.xml")
+
+faces_haar_default =  np.squeeze(faces_haar_default)
+
+#draw face detections
+img_faces_alt2 = show_detection(img.copy(), faces_alt2)
+img_faces_default = show_detection(img.copy(), faces_default)
+img_faces_haar_alt2 = show_detection(img.copy(), faces_haar_alt2)
+img_faces_haar_default = show_detection(img.copy(), faces_haar_default)
+
+
+
+fig = plt.figure(figsize=(10, 8))
+plt.suptitle("Face detection using haar feature-based cascade classifiers", fontsize=14, fontweight='bold')
+fig.patch.set_facecolor('silver')
+
+# Plot the images:
+show_img_with_matplotlib(img_faces_alt2, "detectMultiScale(frontalface_alt2): " + str(len(faces_alt2)), 1)
+show_img_with_matplotlib(img_faces_default, "detectMultiScale(frontalface_default): " + str(len(faces_default)), 2)
+show_img_with_matplotlib(img_faces_haar_alt2, "getFacesHAAR(frontalface_alt2): " + str(len(faces_haar_alt2)), 3)
+show_img_with_matplotlib(img_faces_haar_default, "getFacesHAAR(frontalface_default): " + str(len(faces_haar_default)),
+                         4)
+
+# Show the Figure:
+plt.show()
+
+
+
+
+
+
+
+
+
