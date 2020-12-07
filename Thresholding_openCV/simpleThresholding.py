4db12219cb53241f512178b5a455c06d6bb27903
Pavan Kunchala <39479635+Pavankunchala@users.noreply.github.com>
2020-12-07 21:30:58 +0530

    Simple thresholding

---
+++ Thresholding_openCV/simpleThresholding.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread("img.png",0)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+"""
+In simple thresholding ,For every pixel, the same threshold value is applied.
+"""
+
+ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
+ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
+ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
+ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
+titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
+images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+for i in range(6):
+    plt.subplot(2,3,i+1),plt.imshow(images[i],'gray',vmin=0,vmax=255)
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
